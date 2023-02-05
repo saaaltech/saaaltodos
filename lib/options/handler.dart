32682fbbff2d9bbea83f7e3c98e6594593b323cb
hillzwei <saaaltech@163.com>
2023-02-05 23:27:14 +0800

    feat: value handler bind to states

---
+++ lib/options/handler.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/widgets.dart';
+
+class ValueHandler<T> {
+  ValueHandler(this._value);
+
+  T _value;
+
+  // ignore:unnecessary_getters_setters
+  T get value => _value;
+  set value(T newValue) {
+    _value = newValue;
+  }
+}
+
+class Option<T> extends ValueHandler<T> {
+  Option(super.value);
+
+  final attachedStates = <GlobalKey>[];
+
+  @override
+  set value(T newValue) {
+    if (_value != newValue) {
+      super.value = newValue;
+      for (final key in attachedStates) {
+        // ignore:invalid_use_of_protected_member
+        key.currentState?.setState(() {});
+      }
+    }
+  }
+
+  void attach(GlobalKey key) => attachedStates.add(key);
+  void remove(GlobalKey key) => attachedStates.remove(key);
+  void removeAll() => attachedStates.clear();
+}
