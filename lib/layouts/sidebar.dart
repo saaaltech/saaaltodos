3b2e45ba3f9aeb44b5b199569eaf77918f012fff
hillzwei <saaaltech@163.com>
2023-02-07 21:49:18 +0800

    feat: rtl options into sidebar container

--- lib/layouts/sidebar.dart
+++ lib/layouts/sidebar.dart
@@ -1,21 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:saaaltodos/components/widget_helpers.dart';
+import 'package:saaaltodos/options/preferences.dart';
 
 class SidebarContainer extends StatefulWidget {
   SidebarContainer({
     GlobalKey? key,
     this.sidebar = const CenterText('sidebar'),
     this.mainArea = const CenterText('sidebar container main area'),
-  }) : super(key: key ?? GlobalKey());
+    UserPreference? userPreference,
+  }) : super(key: key ?? GlobalKey()) {
+    this.userPreference = userPreference ?? preference;
+  }
 
   final Widget sidebar;
   final Widget mainArea;
+
+  late final UserPreference userPreference;
 
   @override
   State<SidebarContainer> createState() => _SidebarContainerState();
 }
 
 class _SidebarContainerState extends State<SidebarContainer> {
+  // Aliases as sugars.
+  late final key = widget.key as GlobalKey;
+  late final preference = widget.userPreference;
+
+  /// Current state of locale rtl,
+  /// prevent repeat calling getter to save cost.
+  ///
+  late bool rtl = preference.textDirection.rtl(context);
+
+  @override
+  void initState() {
+    super.initState();
+    preference.textDirection.attach(key);
+  }
+
+  @override
+  void dispose() {
+    preference.textDirection.cancel(key);
+    super.dispose();
+  }
+
+  @override
+  void setState(VoidCallback fn) {
+    // Refresh rtl when locale changed.
+    rtl = preference.textDirection.rtl(context);
+    super.setState(fn);
+  }
+
   Positioned get sidebar {
     return Positioned(
       top: 0,
