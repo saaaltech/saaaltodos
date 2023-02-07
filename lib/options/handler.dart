f6aa51b1a24a0a65b4e3a88ff290d110b2f6e9cb
hillzwei <saaaltech@163.com>
2023-02-07 13:02:05 +0800

    optm: use updater rather than list and map options handler

--- lib/options/handler.dart
+++ lib/options/handler.dart
@@ -7,7 +7,7 @@ class StatesHandler {
   void cancel(GlobalKey key) => attachedStates.remove(key);
   void removeAll() => attachedStates.clear();
 
-  void updateAll() {
+  void updateStates() {
     for (final key in attachedStates) {
       // ignore:invalid_use_of_protected_member
       key.currentState?.setState(() {});
@@ -25,70 +25,12 @@ class Option<T> extends StatesHandler {
   set value(T newValue) {
     if (_value != newValue) {
       _value = newValue;
-      updateAll();
-    }
-  }
-}
-
-class ListOptions<T> extends StatesHandler {
-  ListOptions({List<T>? defaultValue, this.repeatable = false}) {
-    _value = defaultValue ?? [];
-
-    if (!repeatable) {
-      final generator = <T>[];
-      for (final item in _value) {
-        if (!_value.contains(item)) generator.add(item);
-      }
-      _value = generator;
+      updateStates();
     }
   }
 
-  late final List<T> _value;
-
-  late bool repeatable;
-
-  void add(T item) {
-    if (repeatable || !_value.contains(item)) {
-      _value.add(item);
-      updateAll();
-    }
-  }
-
-  void remove(T item, {bool removeAll = false}) {
-    while (_value.remove(item) && removeAll) {}
-    updateAll();
-  }
-
-  void clear() {
-    _value.clear();
-    updateAll();
-  }
-}
-
-class MapOptions<K, V> extends StatesHandler {
-  MapOptions({Map<K, V>? defaultValue}) {
-    _value = defaultValue ?? {};
-  }
-
-  late final Map<K, V> _value;
-
-  void set(K key, V value) {
-    _value[key] = value;
-    updateAll();
-  }
-
-  void sets(Map<K, V> map) {
-    _value.addAll(map);
-    updateAll();
-  }
-
-  void remove(K key) {
-    _value.remove(key);
-    updateAll();
-  }
-
-  void clear() {
-    _value.clear();
-    updateAll();
+  void update(T Function(T) setter) {
+    _value = setter(_value);
+    updateStates();
   }
 }
